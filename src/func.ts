030dca3dbc0a9b7b8ade16d5d8e6fee55c6b0694
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2022-09-09 09:26:31 +0700

    func

--- src/func.ts
+++ src/func.ts
@@ -18,3 +18,41 @@ const Add: HitungFn = (val1: number, val2: number): number => {
     return val1 + val2;
 };
 console.log(Add(2, 3));
+
+const fullName = (
+    firstName: string,
+    middleName?: string,
+    lastName?: string
+): string => {
+    return firstName + middleName + lastName;
+};
+
+type NoArg = {
+    _brand: "NoArg";
+    (): void;
+};
+
+type OneArg = {
+    _brand: "OneArg";
+    (x: number): void;
+};
+
+// Correct typings
+let noArg: NoArg = discriminator(() => {});
+let oneArg: OneArg = discriminator((x: number) => {});
+
+function discriminator<T extends (x: any) => any>(myFunc: T) {
+    let discriminatedFunc;
+    if (myFunc.length === 0) {
+        discriminatedFunc = {
+            _brand: "NoArg",
+            myFunc,
+        };
+    } else {
+        discriminatedFunc = {
+            _brand: "OneArg",
+            myFunc,
+        };
+    }
+    return discriminatedFunc as unknown as T extends () => any ? NoArg : OneArg;
+}
