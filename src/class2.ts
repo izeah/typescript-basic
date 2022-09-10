87447924a1404571534b3fd54c9dd4162f7fa130
Faiz Mohammad Hafidza <faizmohhafidza@gmail.com>
2022-09-10 14:15:26 +0700

    static class, setter, getter

---
+++ src/class2.ts
@@ -0,0 +1,27 @@
+class User {
+    private _email: string = "";
+    static roleName: string = "User";
+
+    set email(val: string) {
+        if (val.length < 5) {
+            this._email = "undefined email";
+        } else {
+            this._email = val;
+        }
+    }
+
+    get email(): string {
+        return this._email;
+    }
+
+    static foo(): void {
+        console.log("Hello, user");
+    }
+}
+
+const user = new User();
+user.email = "izeah@gmail.com";
+
+console.log(user.email);
+console.log(User.roleName);
+User.foo();
